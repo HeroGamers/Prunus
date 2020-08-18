bd2ca49f5154379e5b8aa3e7d0499729ecf44e5c
fido2603 <fido2603gamer@gmail.com>
2020-08-18 14:02:39 +0200

    privacy policy gist

--- cogs/info.py
+++ cogs/info.py
@@ -11,7 +11,7 @@ class Info(commands.Cog):
         @bot.command()
         async def helpme(ctx):
             """Get help"""
-            await ctx.send("No help to get here, but DM Hero if you want the Privacy Policy! :)")
+            await ctx.send("No help to get here, but [click here](https://gist.github.com/HeroGamers/a92b824d899981c4c6c287978a54548c) for the Privacy Policy! :)")
 
 def setup(bot):
     bot.add_cog(Info(bot))
